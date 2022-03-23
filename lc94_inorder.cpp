fbe31128af29c51ea6f6551c3c2a420cded9162b
Shivani Kushwaha <shivi.ymca@gmail.com>
2022-03-23 14:18:44 +0530

    Update lc94_inorder.cpp

--- lc94_inorder.cpp
+++ lc94_inorder.cpp
@@ -13,3 +13,35 @@ public:
         return ans;
     }
 };
+
+//using morris inorder traversal method
+class Solution {
+public:
+    vector<int> ans;
+    vector<int> inorderTraversal(TreeNode* root) {
+        TreeNode* cur = root;
+        while(cur!= NULL){
+            if(cur->left == NULL){
+                ans.push_back(cur->val);
+                cur = cur->right;
+            }
+            else{
+                TreeNode* prev = cur->left;
+                while(prev->right && prev->right != cur){
+                    prev = prev->right;
+                    
+                }
+                if(prev->right == NULL){
+                    prev->right = cur;
+                    cur = cur->left;
+                }
+                else{
+                    prev->right = NULL;
+                    ans.push_back(cur->val);
+                    cur = cur->right;
+                }
+            }
+        }
+        return ans;
+    }
+};
